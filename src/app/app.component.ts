4fa8d1401027b21355826ff94ae4ba2c0f16b143
fjcarnicer <37908048+fjcarnicer@users.noreply.github.com>
2018-03-29 23:31:06 +0200

    otro commit pa la coleccion 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
-
+import { ItemsPage } from '../pages/items/items';
 @Component({
   templateUrl: 'app.html'
 })
@@ -21,8 +21,9 @@ export class MyApp {
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
+      { title: 'Inicio', component: HomePage },
+      { title: 'Listado', component: ListPage },
+      { title: 'Elementos', component: ItemsPage }
     ];
 
   }
---
+++ src/pages/items/items.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-items',
+  templateUrl: 'items.html'
+})
+export class ItemsPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
